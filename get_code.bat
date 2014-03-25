1db7fbec309a9053c610f75224b4e2aa4036a2cd
Duc Duong <duongthienduc@yahoo.com>
2014-03-25 09:51:46 +0700

    Normalize line endings

---
+++ get_code.bat
@@ -0,0 +1,5 @@
+git fetch upstream
+git stash
+git merge upstream/master
+git push origin master
+git stash apply
---
+++ get_code_no_stash.bat
@@ -0,0 +1,5 @@
+git fetch upstream
+rem git stash
+git merge upstream/master
+git push origin master
+rem git stash apply
